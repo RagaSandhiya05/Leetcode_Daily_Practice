debe50278936395a3269ed0917a3a4fac4339c39
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-02-24 19:28:39 +0530

    Time: 1 ms (99.85%) | Memory: 55.7 MB (47.92%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,20 @@
+// Majority Element
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int major = 0;
+        for(int n : nums){
+            if(count == 0){
+                major = n;
+            }
+            if(major == n){
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return major;
+    }
+}
+
+           
