3df46e6659410434dec51b90ad28b7db105b1d0c
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-03-01 14:56:44 +0530

    Time: 138 ms (5.25%) | Memory: 48.4 MB (5.25%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,17 @@
+// Valid Palindrome 
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]" , "");
+        String rev = "";
+        for(int i = s.length() - 1 ; i >= 0 ; i--){
+            rev = rev + s.charAt(i);
+        }
+        if(s.equals(rev)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+       
