345c95df66ced6aa3a88bf299547009dd979e9ed
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-03-08 19:56:32 +0530

    Time: 20 ms (36.76%) | Memory: 91.9 MB (15.54%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+// Find the Duplicate Number
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.contains(num)) {
+                set.add(num);
+            }
+            else {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
+           
