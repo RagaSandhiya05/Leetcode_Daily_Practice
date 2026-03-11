a17c797761c26cfc34011fa211244bfe95bbe94f
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-03-11 22:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (82.81%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,19 @@
+// Complement of Base 10 Integer
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n == 0) 
+            return 1;
+            int ans = 0;
+            int i = 0;
+            while(n > 0) {
+                int bit = n % 2;
+                if(bit == 0) {
+                    ans += Math.pow(2 , i);
+                }
+                i++;
+                n = n / 2;
+            }
+            return ans;
+    }
+}
+               
