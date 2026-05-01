eb825c1887ccb530fed91eeec32486c93274c5c9
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-05-01 10:53:43 +0530

    Time: 4 ms (90.35%) | Memory: 90.4 MB (82.46%) - LeetSync

---
+++ 396-rotate-function/rotate-function.java
@@ -0,0 +1,18 @@
+// Rotate Function
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        long F = 0;
+        for(int i = 0 ; i < n ; i++) {
+            sum = sum + nums[i];
+            F = F + (long)i * nums[i];
+        }
+        long result = F;
+        for(int k = 1 ; k < n ; k++) {
+            F = F + sum - (long)n * nums[n - k];
+            result = Math.max(result , F);
+        }
+        return (int)result;
+    }
+}
