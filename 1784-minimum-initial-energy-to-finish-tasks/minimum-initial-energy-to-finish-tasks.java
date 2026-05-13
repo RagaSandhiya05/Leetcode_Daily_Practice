beb27f55e21bca895a7500bfb3a5b04770dd0eb6
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-05-13 20:43:54 +0530

    Time: 29 ms (22.77%) | Memory: 116.6 MB (7.92%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -0,0 +1,32 @@
+// Minimum Initial Energy to Finish Tasks
+class Solution {
+    static class Pair {
+        int eq;
+        int srq;
+        Pair(int eq, int srq) {
+            this.eq = eq;
+            this.srq = srq;
+        }
+    }
+    public int minimumEffort(int[][] tasks) {
+        List<Pair> list = new ArrayList<>();
+        for (int[] row : tasks) {
+            list.add(new Pair(row[0], row[1]));
+        }
+        list.sort((a, b) -> 
+            Integer.compare((b.srq - b.eq), (a.srq - a.eq))
+        );
+        int engery = 0;
+        int ans = 0;
+        for (Pair cur : list) {
+            int act = cur.eq;
+            int req = cur.srq;
+            if (engery < req) {
+                ans += (req - engery);
+                engery = req;
+            }
+            engery -= act;
+        }
+        return ans;
+    }
+}
