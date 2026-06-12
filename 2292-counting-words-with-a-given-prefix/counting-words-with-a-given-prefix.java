74806cc2d31d6c008e6d060d67188981b142e45e
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-12 18:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (76.61%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.java
@@ -0,0 +1,13 @@
+// Counting Words With a Given Prefix
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        int n = pref.length();
+        for(String w : words) {
+            if(w.length() >= n && w.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
