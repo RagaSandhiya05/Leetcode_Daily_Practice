74f68d217f5774f43442903a54b2f7cfa8b68063
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-12 22:10:03 +0530

    Time: 4 ms (40.04%) | Memory: 43.1 MB (96.77%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -0,0 +1,18 @@
+// Uncommon Words from Two Sentences
+class Solution {
+    public String[] uncommonFromSentences(String s1 , String s2) {
+        String str = s1 + " " + s2;
+        Map<String, Integer> wordCount = new HashMap<>();
+        String[] words = str.split(" ");
+        for (String word : words) {
+            wordCount.put(word , wordCount.getOrDefault(word , 0) + 1);
+        }
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String , Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                result.add(entry.getKey());
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
