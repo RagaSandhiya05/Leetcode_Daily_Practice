7515dca43aee5e221805f5a0d947508331af364c
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-14 23:04:55 +0530

    Time: 2 ms (26.86%) | Memory: 42.8 MB (60.86%) - LeetSync

---
+++ 522-longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.java
@@ -0,0 +1,30 @@
+// Longest Uncommon Subsequence II
+class Solution {
+    public int findLUSlength(String[] strs) {
+        Arrays.sort(strs , (a , b) -> b.length() - a.length());
+        for(int i = 0 ; i < strs.length ; i++) {
+            boolean isUncommon = true;
+            for(int j = 0 ; j < strs.length ; j++) {
+                if(i != j && isSubsequence(strs[i] , strs[j])) {
+                    isUncommon = false;
+                    break;
+                }
+            }
+            if(isUncommon) {
+                return strs[i].length();
+            }
+        }
+        return -1;
+    }
+    private static boolean isSubsequence(String a , String b) {
+        int i = 0;
+        int j = 0;
+        while(i < a.length() && j < b.length()) {
+        if(a.charAt(i) == b.charAt(j)) {
+            i++;
+        }
+        j++;
+    }
+    return i == a.length();
+    }
+}
