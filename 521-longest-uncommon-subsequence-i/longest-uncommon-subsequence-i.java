fc8c49057138fda3f43f3d4e37b4f7231b2ec97d
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-14 22:57:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (88.66%) - LeetSync

---
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
@@ -0,0 +1,6 @@
+// Longest Uncommon Subsequence I
+class Solution {
+    public int findLUSlength(String a, String b) {
+        return a.equals(b) ? -1 : Math.max(a.length() , b.length());
+    }
+}
