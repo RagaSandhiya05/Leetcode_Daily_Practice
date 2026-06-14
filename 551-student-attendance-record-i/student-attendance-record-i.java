4f00cc81aa1b768ceeef4ed82a05c118f6afb0ee
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-14 23:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (25.16%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,18 @@
+// Student Attendance Record I
+class Solution {
+    public boolean checkRecord(String s) {
+        int countA = 0;
+        int countL = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch != 'L')
+            countL = 0;
+            if(ch == 'L')
+            countL++;
+            if(ch == 'A')
+            countA++;
+            if(countA == 2 || countL == 3)
+            return false;
+        }
+        return true;
+    }
+}
