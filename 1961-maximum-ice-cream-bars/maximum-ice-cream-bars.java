8f3cab6514dd41aa76b4220a12d93dc3fbad4e7b
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-21 12:55:45 +0530

    Time: 36 ms (77.33%) | Memory: 84 MB (43.59%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -0,0 +1,14 @@
+// Maximum Ice Cream Bars
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int ans = 0;
+        for(int cost : costs) {
+            if(cost > coins)
+            break;
+            ans++;
+            coins = coins - cost;
+        }
+        return ans;
+    }
+}
