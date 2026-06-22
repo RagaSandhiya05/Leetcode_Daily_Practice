fb4d48cc809ea0f95cb69e401d311010b3139501
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-22 16:16:15 +0530

    Time: 1 ms (68.00%) | Memory: 45.1 MB (61.33%) - LeetSync

---
+++ 3998-minimum-operations-to-equalize-array/minimum-operations-to-equalize-array.java
@@ -0,0 +1,10 @@
+// Minimum Operations to Equalize Array
+class Solution {
+    public int minOperations(int[] nums) {
+        for(int i = 1 ; i < nums.length ; i++) {
+            if(nums[i] != nums[i - 1]) 
+                return 1;
+            }
+            return 0;
+    }
+}
