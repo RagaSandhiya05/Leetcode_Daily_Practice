bae12785479a64284a225204ddc92ed58d09ee2b
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-27 11:30:01 +0530

    Time: 2 ms (88.84%) | Memory: 47.8 MB (80.04%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,12 @@
+// Jump Game
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length -1;
+        for(int i = nums.length - 1 ; i >= 0 ; i--) {
+            if(i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+}
