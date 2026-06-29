5c5d78ac7de4348a9f0147e81787246f44138afd
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-29 09:37:20 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (68.18%) - LeetSync

---
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.java
@@ -0,0 +1,15 @@
+// Sum of Squares of Special Elements
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        for(int i = 1 ; i <= n ; i++) {
+            int num = nums[i - 1];
+            if(n % i == 0){
+                sum = sum + num * num;
+            }
+        }
+        return sum;
+    }
+}
+           
