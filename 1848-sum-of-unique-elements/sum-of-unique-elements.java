4b71b07eaf30cf9ede64538cdf50e85f1a19a7a8
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-06-30 12:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (73.69%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -0,0 +1,21 @@
+// Sum of Unique Elements
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i < nums.length ; i++) {
+            max = Math.max(max , nums[i]);
+        }
+        int count[] = new int[max + 1];
+        for(int i = 0 ; i < nums.length ; i++) {
+            count[nums[i]]++;
+        }
+        for(int i = 0 ; i < count.length ; i++) {
+            if(count[i] == 1) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+}
+           
