769e2c5c49110f90580ebcf0e46dd80c080a5856
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-08 14:43:55 +0530

    Time: 2 ms (76.78%) | Memory: 43.3 MB (63.65%) - LeetSync

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -0,0 +1,14 @@
+// Check if All Characters Have Equal Number of Occurences
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        final int[] counts = new int[26];
+        for(int i = 0 ; i < s.length() ; i++)
+        counts[s.charAt(i) - 'a']++;
+        int count = counts[s.charAt(0) - 'a'];
+        for(int i = 1 ; i < counts.length ; i++)
+        if(count != counts[i] && counts[i] != 0)
+        return false;
+        return true;
+    }
+}
+    
