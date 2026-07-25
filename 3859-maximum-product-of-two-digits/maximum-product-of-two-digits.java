0edfe9eb7965ffbad279d7fc6c30c6a0892cd15f
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-25 08:58:01 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (58.74%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int n) {
+        int digit1 = 0;
+        int digit2 = 0;
+        while(n > 0) {
+            int rem = n % 10;
+            if(rem >= digit1) {
+                digit2 = digit1;
+                digit1 = rem;
+            }
+            else if(rem > digit2) {
+                digit2 = rem;
+            }
+            n = n / 10;
+        }
+        return digit1 * digit2;
+    }
+}
+          
