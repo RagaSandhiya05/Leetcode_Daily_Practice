8a709470023f00f17a28ef175acd1fd1bcc29e84
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-28 08:55:56 +0530

    Time: 22 ms (91.16%) | Memory: 48.2 MB (57.43%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,25 @@
+// Smallest Palindromic Rearrangement I
+class Solution {
+    public String smallestPalindrome(String s) {
+        int freq[] = new int[26];
+        for(char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        int n = s.length();
+        char ans[] = new char[n];
+        int left = 0;
+        int right = n - 1;
+        for(int i = 0 ; i < 26 ; i++) {
+            while(freq[i] >= 2) {
+                ans[left++] = (char) ('a' + i);
+                ans[right--] = (char) ('a' + i);
+                freq[i] = freq[i] - 2;
+            }
+            if(freq[i] == 1) {
+                ans[n / 2] = (char) ('a' + i);
+            }
+        }
+        return new String(ans);
+    }
+}
+           
