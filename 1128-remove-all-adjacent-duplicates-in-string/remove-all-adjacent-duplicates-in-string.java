cd318902bca07ff8a870173db22b594714453d6c
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-31 09:10:35 +0530

    Time: 23 ms (69.54%) | Memory: 46.8 MB (86.91%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,20 @@
+// Remove All Adjacent Duplicates In String
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()) {
+            if(!stack.isEmpty() && ch == stack.peek()) {
+                stack.pop();
+            }
+            else {
+                stack.push(ch);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(char ch : stack) {
+            result.append(ch);
+        }
+        return result.toString();
+    }
+}
+       
