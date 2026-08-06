a56f1800aa33bca8a2fe887fb0511053e28c7d4a
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-08-06 12:06:57 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (30.89%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,17 @@
+// Smallest Divisible Digit Product I
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(true) {
+            int product = 1;
+            int x = n;
+            while(x > 0) {
+                product = product * (x % 10);
+                x = x / 10;
+            }
+            if(product % t == 0)
+            return n;
+            n++;
+        }
+    }
+}
+           
